bdca7622ef5d143bffd6140ad72421db70373519
Robin Munn <rmunn@pobox.com>
2021-03-31 13:57:36 +0700

    Use mdsvex to have a page in Markdown
    Yes, it's that easy

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,11 @@
 const node = require('@sveltejs/adapter-node');
 const pkg = require('./package.json');
+const { mdsvex } = require('mdsvex');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	extensions: ['.svelte', '.svx'],
+	preprocess: [ mdsvex() ],
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
